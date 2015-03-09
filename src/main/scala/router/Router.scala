13d9e2390f918d61bb7afa127c426fe5cd48a30b
pamu <nagarjuna.pamu@gmail.com>
2015-03-09 14:45:34 +0530

    Router code programmatically

--- src/main/scala/router/Router.scala
+++ src/main/scala/router/Router.scala
@@ -1,7 +1,7 @@
 package router
 
-import akka.actor.{Props, ActorSystem}
-import akka.routing.FromConfig
+import akka.actor.{ActorRef, Props, ActorSystem}
+import akka.routing.{ConsistentHashingGroup, FromConfig}
 import com.typesafe.config.ConfigFactory
 import node.Node
 
@@ -10,6 +10,10 @@ import node.Node
  */
 object Router {
   def main(args: Array[String]): Unit = {
+    val paths = List("akka.tcp://NodeSystem@127.0.0.1:2551",
+      "akka.tcp://NodeSystem@127.0.0.1:2552")
+    val router: ActorRef = ActorSystem("NodeSystem").
+      actorOf(ConsistentHashingGroup(paths).props(), "router")
 
   }
 }
