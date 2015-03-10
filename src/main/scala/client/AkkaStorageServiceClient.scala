3423f4e427a5a567400beccd8a641e3a1372a8cf
pamu <nagarjuna.pamu@gmail.com>
2015-03-10 16:14:37 +0530

    ass client added

--- src/main/scala/client/AkkaStorageServiceClient.scala
+++ src/main/scala/client/AkkaStorageServiceClient.scala
@@ -1,23 +1,49 @@
 package client
 
-import akka.actor.{ActorLogging, Actor}
+import akka.actor._
+import akka.cluster.Cluster
+import akka.cluster.ClusterEvent.{ReachabilityEvent, MemberEvent}
+import scala.concurrent.duration._
 
 /**
  * Created by android on 10/3/15.
  */
-object AkkaStorageServiceClient {
-  case object Start
-}
 
-class AkkaStorageServiceClient extends Actor with ActorLogging {
-  import AkkaStorageServiceClient._
+class AkkaStorageServiceClient(servicePath: String) extends Actor with ActorLogging {
+  import worker._
+
+  val cluster = Cluster(context.system)
+
+  val servicePathElements = servicePath match {
+    case RelativeActorPath(elements) => elements
+    case _ => throw new IllegalArgumentException(
+      "servicePath [%s] is not a valid relative error path" format servicePath
+    )
+  }
+
+  override def preStart(): Unit = {
+    cluster.subscribe(self, classOf[MemberEvent], classOf[ReachabilityEvent])
+  }
+
+  override def postStop(): Unit = {
+    cluster.unsubscribe(self)
+  }
+
+  import context.dispatcher
+  val tickTask = context.system.scheduler.schedule(2.seconds, 2.seconds, self, "tick")
+
+  var nodes = Set.empty[Address]
+
   override def receive = {
-    case Start => log.info("Started Ass Client")
+    case "tick" if nodes.isEmpty => {
+
+    }
   }
 }
 
 object Starter {
   def main(args: Array[String]): Unit = {
     //start the ass client
+
   }
 }
