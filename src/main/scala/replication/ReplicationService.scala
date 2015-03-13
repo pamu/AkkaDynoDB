7b8cc12ef19e6effef76f2999f8f9dc0aa69ef1e
pamu <nagarjuna.pamu@gmail.com>
2015-03-14 00:31:00 +0530

    replication actor

--- src/main/scala/replication/ReplicationService.scala
+++ src/main/scala/replication/ReplicationService.scala
@@ -1,8 +1,21 @@
 package replication
 
+import akka.actor.Actor
+
 /**
  * Created by android on 14/3/15.
  */
 object ReplicationService {
+  case object Replicate
+}
 
+class ReplicationService extends Actor {
+
+  import ReplicationService._
+
+  def receive = {
+    case Replicate => {
+
+    }
+  }
 }
