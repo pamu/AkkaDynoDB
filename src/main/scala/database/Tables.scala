745666d8816341b8b82320625edb88d0f782797c
pamu <nagarjuna.pamu@gmail.com>
2015-03-31 22:47:00 +0530

    cleaning code

--- src/main/scala/database/Tables.scala
+++ src/main/scala/database/Tables.scala
@@ -10,8 +10,8 @@ import scala.slick.driver.MySQLDriver.simple._
  * Created by android on 11/3/15.
  */
 
+/**
 object Tables {
-  /**
   case class User(firstName: String, lastName: String, email: String,
                   time: Timestamp, id: Option[Long] = None)
   class Users(tag: Tag) extends Table[User](tag, "users") {
@@ -22,5 +22,6 @@ object Tables {
     def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
     def * = (firstName, lastName, email, time, id.?) <> (User.tupled, User.unapply)
   }
-  val users = TableQuery[Users] **/
+  val users = TableQuery[Users]
 }
+**/
