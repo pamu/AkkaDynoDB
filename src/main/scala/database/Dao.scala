4c4032ede2c648112bce68764d52e3cac05047a1
pamu <nagarjuna.pamu@gmail.com>
2015-03-31 22:48:36 +0530

    cleaning code

--- src/main/scala/database/Dao.scala
+++ src/main/scala/database/Dao.scala
@@ -4,6 +4,7 @@ package database
  * Created by android on 11/3/15.
  */
 
+/**
 import database.Tables._
 
 import scala.slick.driver.MySQLDriver.simple._
@@ -15,4 +16,4 @@ object Dao {
   def save(user: User): Unit = Db.db.withSession(implicit sx => {
     Tables.users += user
   })
-}
+} **/
