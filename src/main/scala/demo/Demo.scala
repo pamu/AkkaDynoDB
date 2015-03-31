02ac4adf266ce1469d2bcad22e024132bd25377a
pamu <nagarjuna.pamu@gmail.com>
2015-04-01 04:43:03 +0530

    active slick

--- src/main/scala/demo/Demo.scala
+++ src/main/scala/demo/Demo.scala
@@ -9,12 +9,41 @@ import scala.slick.driver.MySQLDriver.simple._
  */
 
 object Demo {
-  final case class Save[M](tableQuery: TableQuery[Table[M]], model: M)
+  final case class Entry[M](tableQuery: TableQuery[Table[M]], model: M)
+  final case class Get[M](tableQuery: TableQuery[Table[M]], id: Long)
+  final case class Evict[M](tableQuery: TableQuery[Table[M]], id: Long)
 }
 
 class Demo extends Actor {
   import Demo._
+  import database.Db._
   def receive = {
-    case Save(tableQuery, model) =>
+    case Entry(tableQuery, model) => {
+      db.withSession {
+        implicit sx => tableQuery += model
+      }
+    }
+    case Get(tableQuery, id) => {
+      db.withSession {
+        implicit sx => {
+          val query = for(model <- tableQuery.filter(_.id === id)) yield model
+          query.firstOption
+        }
+      }
+    }
+    case Evict(tableQuery, id) => {
+      db.withSession {
+        implicit sx => {
+          val query = for(model <- tableQuery.filter(_.id === id)) yield model
+          query.delete
+        }
+      }
+    }
+  }
+}
+
+object Starter {
+  def main(args: Array[String]): Unit = {
+
   }
 }
